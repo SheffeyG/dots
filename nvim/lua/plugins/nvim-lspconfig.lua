7290fe87145380638670bdd7202af572753708c8
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-06-21 15:49:08 +0800

    nvim.lsp: remove pylsp last blank line warning

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -25,7 +25,7 @@ return {
             pylsp = {
               plugins = {
                 pycodestyle = {
-                  ignore = {'E401'},
+                  ignore = {'E401', 'W391'},
                   maxLineLength = 150
                 }
               }
