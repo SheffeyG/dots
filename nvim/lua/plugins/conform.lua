3720ae131199504a4e461f5870ebd33d39f78852
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-08-24 15:02:19 +0800

    nvim: polish conform config

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,27 +1,23 @@
-local options = {
-    formatters_by_ft = {
-        c = { "clang-format" },
-        cpp = { "clang-format" },
-        css = { "prettier" },
-        javascript = { "prettier" },
-        html = { "prettier" },
-        lua = { "stylua" },
-        markdown = { "prettier" },
-        python = { "ruff" },
-        typescript = { "prettier" },
-    },
+return {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opt = {
+        formatters_by_ft = {
+            c = { "clang-format" },
+            cpp = { "clang-format" },
+            css = { "prettier" },
+            javascript = { "prettier" },
+            html = { "prettier" },
+            lua = { "stylua" },
+            markdown = { "prettier" },
+            python = { "ruff" },
+            typescript = { "prettier" },
+        },
 
     -- format_on_save = {
     --   -- These options will be passed to conform.format()
     --   timeout_ms = 500,
     --   lsp_fallback = true,
     -- },
-}
-
-return {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opt = {
-        require("conform").setup(options),
     },
 }
