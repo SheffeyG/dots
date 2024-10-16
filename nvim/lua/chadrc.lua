b51fd8ea301419b5f9e35502e355f57f19a31c5d
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-10-16 10:01:55 +0800

    nvim: simplify chadrc

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,16 +1,10 @@
--- This file needs to have same structure as nvconfig.lua
--- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
-
----@type ChadrcConfig
-local M = {}
-
-M.base64 = {
-    theme = "onedark",
-    -- transparency = true,
-    -- hl_override = {
-    -- 	Comment = { italic = true },
-    -- 	["@comment"] = { italic = true },
-    -- },
+return {
+    mason = {
+        pkgs = {
+            "ruff",
+            "lua_ls",
+            "prettier",
+        }
+    }
 }
 
-return M
