cae208f82b8fa98c890a39991eb9a924d18ecbec
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-10-26 15:27:28 +0800

    nvim: replace deprecated vim api

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -5,7 +5,7 @@ autocmd("BufReadPost", {
     pattern = { "*" },
     callback = function()
         if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
-            vim.api.nvim_exec("normal! g'\"", false)
+            vim.cmd("normal! g'\"", false)
         end
     end,
 })
