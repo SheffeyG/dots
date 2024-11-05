5abd483919a93d320140650966bb62e02396f97f
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-11-05 16:58:01 +0800

    nvim: fix treesitter config setup
    something magic happend inside treesitter config setup!

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,34 +3,40 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
     build = ":TSUpdate",
-    opts = function()
-        dofile(vim.g.base46_cache .. "syntax")
-        dofile(vim.g.base46_cache .. "treesitter")
-        return {
-            ensure_installed = {
-                "c",
-                "cpp",
-                "python",
-                "lua",
-                "luadoc",
-                "vim",
-                "vimdoc",
-                "html",
-                "css",
-                "jsvascript",
-                "json",
-                "yaml",
-                "toml",
-                "printf",
-                "markdown",
-            },
-            sync_install = true,
-            auto_install = false,
-            highlight = {
-                enable = true,
-                use_languagetree = true,
-            },
-            indent = { enable = true },
-        }
+
+    opts = {
+        ensure_installed = {
+            "c",
+            "cpp",
+            "python",
+            "lua",
+            "luadoc",
+            "vim",
+            "vimdoc",
+            "html",
+            "css",
+            "javascript",
+            "json",
+            "yaml",
+            "toml",
+            "printf",
+            "markdown",
+        },
+        sync_install = true,
+        auto_install = false,
+        highlight = {
+            enable = true,
+            use_languagetree = true,
+        },
+        indent = { enable = true },
+    },
+
+    config = function(_, opts)
+        pcall(function()
+            dofile(vim.g.base46_cache .. "syntax")
+            dofile(vim.g.base46_cache .. "treesitter")
+        end)
+
+        require("nvim-treesitter.configs").setup(opts)
     end,
 }
