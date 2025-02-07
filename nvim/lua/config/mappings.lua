047e9b055acf73658702048006f687f7474bc741
sheffey <57262511+SheffeyG@users.noreply.github.com>
2025-02-07 15:54:27 +0800

    cleaner options

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,25 +1,27 @@
--- stylua: ignore start
-
 local map = vim.keymap.set
 
-------------------------------------- General ----------------------------------------
+--------------------------
+--- General
+--------------------------
 
 map("n", ";", ":")
 map("i", "jk", "<ESC>")
 
 map("n", "<ESC>", "<cmd>noh<CR>", { desc = "General clear highlights" })
-map("n", "<C-a>", "ggVG",         { desc = "General select whole file" })
+map("n", "<C-a>", "ggVG", { desc = "General select whole file" })
 
 map("n", "<leader>/", "gcc", { desc = "Toggle comment", remap = true })
-map("v", "<leader>/", "gc",  { desc = "Toggle comment", remap = true })
+map("v", "<leader>/", "gc", { desc = "Toggle comment", remap = true })
 
--------------------------------------- Move ------------------------------------------
+--------------------------
+--- Edit
+--------------------------
 
 map("i", "<C-b>", "<ESC>^i", { desc = "Move cusor beginning of line" })
-map("i", "<C-e>", "<End>",   { desc = "Move cusor end of line" })
-map("i", "<C-h>", "<Left>",  { desc = "Move cusor left" })
-map("i", "<C-j>", "<Down>",  { desc = "Move cusor down" })
-map("i", "<C-k>", "<Up>",    { desc = "Move cusor up" })
+map("i", "<C-e>", "<End>", { desc = "Move cusor end of line" })
+map("i", "<C-h>", "<Left>", { desc = "Move cusor left" })
+map("i", "<C-j>", "<Down>", { desc = "Move cusor down" })
+map("i", "<C-k>", "<Up>", { desc = "Move cusor up" })
 map("i", "<C-l>", "<Right>", { desc = "Move cusor right" })
 
 map("n", "H", "^", { desc = "Move cusor beginning of line" })
@@ -30,15 +32,16 @@ map("n", "<C-j>", "<C-w>j", { desc = "Switch window down" })
 map("n", "<C-k>", "<C-w>k", { desc = "Switch window up" })
 map("n", "<C-l>", "<C-w>l", { desc = "Switch window right" })
 
------------------------------------- Terminal ----------------------------------------
+--------------------------
+--- Buffers
+--------------------------
 
 map("t", "<C-x>", "<C-\\><C-N>", { desc = "Terminal escape terminal mode" })
-
-------------------------------------- Buffer -----------------------------------------
-
 map("n", "<leader>b", "<cmd>enew<CR>", { desc = "Buffer new buffer" })
 
-------------------------------------- Plugins ----------------------------------------
+--------------------------
+--- Plugins
+--------------------------
 
 -- conform
 map("n", "<leader>fm", function()
@@ -46,7 +49,7 @@ map("n", "<leader>fm", function()
 end, { desc = "General format file" })
 
 -- nvimtree
-map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>",    { desc = "Nvimtree toggle window" })
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree toggle window" })
 map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "Nvimtree focus window" })
 
 -- noice
@@ -55,20 +58,12 @@ map("n", "<leader>mh", function()
 end, { desc = "Noice show message history" })
 
 -- telescope
-map("n", "<leader>ff", "<cmd>Telescope find_files<CR>",  { desc = "Telescope find files" })
-map("n", "<leader>fg", "<cmd>Telescope live_grep<CR>",   { desc = "Telescope live grep" })
-map("n", "<leader>fb", "<cmd>Telescope buffers<CR>",     { desc = "Telescope find buffers" })
-map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>",   { desc = "Telescope help page" })
-map("n", "<leader>ft", "<cmd>Telescope terms<CR>",       { desc = "Telescope find hidden term" })
-map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>",    { desc = "Telescope find oldfiles" })
-map("n", "<leader>ma", "<cmd>Telescope marks<CR>",       { desc = "Telescope find marks" })
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Telescope find files" })
+map("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Telescope live grep" })
+map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "Telescope find buffers" })
+map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Telescope help page" })
+map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "Telescope find oldfiles" })
+map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "Telescope find marks" })
 map("n", "<leader>gm", "<cmd>Telescope git_commits<CR>", { desc = "Telescope git commits" })
-map("n", "<leader>gt", "<cmd>Telescope git_status<CR>",  { desc = "Telescope git status" })
-
-map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>",
-    { desc = "Telescope find in current buffer" })
-
-map("n", "<leader>fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
-    { desc = "Telescope find all files" })
-
--- stylua: ignore end
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "Telescope git status" })
+map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "Telescope fuzzy find" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,29 +2,31 @@ local opt = vim.opt
 local o = vim.o
 local g = vim.g
 
+o.mouse = "a"
 o.laststatus = 3
 o.showtabline = 0
-o.clipboard = "unnamedplus"
 o.cursorline = true
+o.clipboard = "unnamedplus"
 o.relativenumber = false
+o.ignorecase = true
+o.smartcase = true
 
---- indenting ---
+-- indents
 o.expandtab = true
 o.shiftwidth = 4
 o.smartindent = true
 o.tabstop = 4
 o.softtabstop = 4
 
-o.ignorecase = true
-o.smartcase = true
-o.mouse = "a"
-
---- numbers ---
+-- numbers
 o.number = true
 o.numberwidth = 2
 
 -- disable nvim intro
 opt.shortmess:append("sI")
+
+-- go to previous/next when reached the beginning/end of line
+opt.whichwrap:append("<>[]hl")
 
 opt.fillchars:append({ eob = " " })
 opt.fillchars:append({ diff = "╱" })
@@ -38,10 +40,7 @@ o.undofile = true
 -- interval for writing swap file to disk, also used by gitsigns
 o.updatetime = 250
 
--- go to previous/next when reached the beginning/end of line
-opt.whichwrap:append("<>[]hl")
-
--- disable some default providers
+-- disable some providers
 g.loaded_node_provider = 0
 g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
