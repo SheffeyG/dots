b808be6f2e71299e94343308b7466aa622a366cf
sheffey <57262511+SheffeyG@users.noreply.github.com>
2025-02-18 09:57:01 +0800

    nvim: add lualine colors
    * use filetype as filename icon

--- nvim/lua/plugins/ui/lualine.lua
+++ nvim/lua/plugins/ui/lualine.lua
@@ -1,17 +1,66 @@
+local colors = require("colors.lualine")
+
+-- conditions
+local many_tabs = function() return #vim.api.nvim_list_tabpages() > 1 end
+-- local not_empty = function() return vim.fn.empty(vim.fn.expand("%:t")) ~= 1 end
+-- local wide_win = function() return vim.fn.winwidth(0) > 80 end
+-- local has_lsp = function() return next(vim.lsp.get_clients()) ~= nil end
+
+-- components
+local filename = {
+    "filetype",
+    fmt = function() return vim.fn.expand("%:t") end,
+    padding = { left = 1, right = 0 },
+}
+
+local diff = {
+    "diff",
+    symbols = { added = " ", modified = " ", removed = " " },
+    diff_color = colors.diff,
+    padding = { left = 1, right = 0 },
+}
+
+local diagnostics = {
+    "diagnostics",
+    symbols = { error = " ", warn = " ", hint = " ", info = " " },
+    sections = { "error", "warn", "info", "hint" },
+    padding = { left = 0, right = 1 },
+    diagnostics_color = colors.diagnostics,
+}
+
+local macro = {
+    "macro",
+    icon = "",
+    fmt = function()
+        local reg = vim.fn.reg_recording()
+        if reg ~= "" then return reg end
+        return nil
+    end,
+    color = colors.text_red,
+    padding = { left = 0, right = 1 },
+}
+
+local select = {
+    "selectioncount",
+    icon = "󰒉",
+    color = colors.text_purple,
+    padding = { left = 0, right = 1 },
+}
+
 --- @type LazyPluginSpec
 return {
     "nvim-lualine/lualine.nvim",
     lazy = false,
     dependencies = {
         "nvim-tree/nvim-web-devicons",
-        "yavorski/lualine-macro-recording.nvim",
     },
     opts = {
         options = {
-            theme = "onedark",
+            theme = colors.theme,
             section_separators = "",
             component_separators = "",
-            ignore_focus = { "NeogitStatus" },
+            disabled_filetypes = { "Lazy", "Mason" },
+            ignore_focus = { "Lazy", "Mason", "NeogitStatus" },
             globalstatus = true,
         },
         sections = {
@@ -22,19 +71,19 @@ return {
                 { "branch", icon = "" },
             },
             lualine_c = {
-                { "filetype", icon_only = true, padding = { left = 1, right = 0 } },
-                { "filename", padding = 0 },
+                filename,
+                diff,
             },
             lualine_x = {
-                { "macro_recording", padding = { left = 0, right = 1 } },
-                { "selectioncount", padding = { left = 0, right = 1 } },
-                { "encoding", padding = { left = 0, right = 1 } },
+                select,
+                macro,
+                diagnostics,
             },
             lualine_y = {
-                "progress",
+                { "progress", color = colors.text_blue },
             },
             lualine_z = {
-                { "tabs", cond = function() return #vim.api.nvim_list_tabpages() > 1 end },
+                { "tabs", show_modified_status = false, cond = many_tabs },
             },
         },
     },
