0b0c81608b5bd02a70e72595df0f9c6e1dc20270
SheffeyG <57262511+SheffeyG@users.noreply.github.com>
2025-10-14 18:11:21 +0800

    nvim: more lsp keymaps

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,5 +1,24 @@
 local icons = require("custom.icons").diag
 
+--- LSP ------------------------------------------
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+    desc = "Enable all LSP servers",
+    once = true,
+    callback = function()
+        local servers = {
+            "basedpyright",
+            "bashls",
+            "clangd",
+            "lua_ls",
+            "rust_analyzer",
+        }
+        -- local servers = vim.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
+        --     :map(function(file) return vim.fn.fnamemodify(file, ":t:r") end)
+        --     :totable()
+        vim.lsp.enable(servers)
+    end,
+})
+
 --- Diagnostic -----------------------------------
 vim.diagnostic.config({
     signs = { text = { icons.Error, icons.Warn, icons.Hint, icons.Info } },
@@ -13,22 +32,11 @@ vim.diagnostic.config({
     -- },
 })
 
---- LSP ------------------------------------------
-vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
-    once = true,
-    callback = function()
-        local server_configs = vim.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
-            :map(function(file) return vim.fn.fnamemodify(file, ":t:r") end)
-            :totable()
-        vim.lsp.enable(server_configs)
-    end,
-})
-
 --- Fold -----------------------------------------
 vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "Use LSP as fold method",
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
-        -- set lsp as fold method if possiable
         if client and client:supports_method("textDocument/foldingRange") then
             local win = vim.api.nvim_get_current_win()
             vim.wo[win][0].foldmethod = "expr"
@@ -39,27 +47,45 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 vim.api.nvim_create_autocmd("LspDetach", { command = "setl foldexpr<" })
 
---- NES ------------------------------------------
+--- Keymaps --------------------------------------
+---@param client vim.lsp.Client
+---@param bufnr integer
+local function on_attach(client, bufnr)
+    ---@param lhs string
+    ---@param rhs string|function
+    ---@param desc? string
+    ---@param mode? string|string[]
+    local function keymap(lhs, rhs, desc, mode)
+        mode = mode or "n"
+        vim.keymap.set(mode, lhs, rhs, {
+            desc = "LSP: " .. desc,
+            buffer = bufnr,
+        })
+    end
+
+    -- if client:supports_method("textDocument/onTypeFormatting", bufnr) then
+    --     vim.lsp.on_type_formatting.enable(true, { bufnr = bufnr })
+    -- end
+
+    if client:supports_method("textDocument/inlineCompletion", bufnr) then
+        vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
+        keymap("<C-y>", vim.lsp.inline_completion.get, "Accept NES", "i")
+        keymap("<C-n>", vim.lsp.inline_completion.select, "Switch NES", "i")
+    end
+
+    if client:supports_method("textDocument/codeAction") then
+        keymap("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+    end
+
+    if client:supports_method("textDocument/rename") then --
+        keymap("<leader>rn", vim.lsp.buf.rename, "Rename")
+    end
+end
+
 vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "Configure LSP keymaps",
     callback = function(args)
-        local bufnr = args.buf
-        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-
-        if client:supports_method("textDocument/inlineCompletion", bufnr) then
-            vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
-
-            vim.keymap.set(
-                "i",
-                "<C-y>",
-                vim.lsp.inline_completion.get,
-                { desc = "LSP: accept inline completion", buffer = bufnr }
-            )
-            vim.keymap.set(
-                "i",
-                "<C-n>",
-                vim.lsp.inline_completion.select,
-                { desc = "LSP: switch inline completion", buffer = bufnr }
-            )
-        end
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client then on_attach(client, args.buf) end
     end,
 })
--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,57 +1,53 @@
-local map = vim.keymap.set
+local keymap = vim.keymap.set
 
--- disable useless number auto-in(de)crement
-map("", "<C-a>", "<Nop>", { unique = true, desc = "which_key_ignore" })
-map("", "<C-x>", "<Nop>", { unique = true, desc = "which_key_ignore" })
+-- Disable some useless action
+keymap("", "<C-a>", "<Nop>", { unique = true, desc = "which_key_ignore" })
+keymap("", "<C-x>", "<Nop>", { unique = true, desc = "which_key_ignore" })
 
--- shortcut alias
-map("i", "jk", "<Esc>", { desc = "Esc Alias" })
-map("n", "U", "<C-r>", { desc = "Redo Alias" })
-map("n", "<Esc>", "<CMD>noh<CR>", { desc = "Clear highlights" })
-map("n", "<C-a>", "<CMD>keepjumps normal! ggVG<CR>", { desc = "Select All" })
+-- Shortcut aliases
+keymap("i", "jk", "<Esc>", { desc = "Esc Alias" })
+keymap("n", "U", "<C-r>", { desc = "Redo Alias" })
+keymap("n", "<Esc>", "<CMD>noh<CR>", { desc = "Clear highlights" })
+keymap("n", "<C-a>", "<CMD>keepjumps normal! ggVG<CR>", { desc = "Select All" })
 
--- comment
-map("n", "<leader>/", "gcc", { remap = true, desc = "Toggle comment" })
-map("x", "<leader>/", "gc", { remap = true, desc = "Toggle comment" })
+-- Comment
+keymap("n", "<leader>/", "gcc", { remap = true, desc = "Toggle comment" })
+keymap("x", "<leader>/", "gc", { remap = true, desc = "Toggle comment" })
 
--- cursor
-map("i", "<C-h>", "<Left>", { desc = "Cusor go Left" })
-map("i", "<C-j>", "<Down>", { desc = "Cusor go Down" })
-map("i", "<C-k>", "<Up>", { desc = "Cusor go Up" })
-map("i", "<C-l>", "<Right>", { desc = "Cusor go Right" })
+-- Move cursor
+keymap("i", "<C-h>", "<Left>", { desc = "Cusor go Left" })
+keymap("i", "<C-j>", "<Down>", { desc = "Cusor go Down" })
+keymap("i", "<C-k>", "<Up>", { desc = "Cusor go Up" })
+keymap("i", "<C-l>", "<Right>", { desc = "Cusor go Right" })
 
-map({ "n", "x", "o" }, "H", "^", { desc = "Home" })
-map({ "n", "x", "o" }, "L", "$", { desc = "End" })
+keymap({ "n", "x", "o" }, "H", "^", { desc = "Home" })
+keymap({ "n", "x", "o" }, "L", "$", { desc = "End" })
 
--- line
-map("n", "<A-j>", "<CMD>m .+1<CR>==", { desc = "Line go Down" })
-map("n", "<A-k>", "<CMD>m .-2<CR>==", { desc = "Line go Up" })
+-- Move lines
+keymap("n", "<A-j>", "<CMD>m .+1<CR>==", { desc = "Line go Down" })
+keymap("n", "<A-k>", "<CMD>m .-2<CR>==", { desc = "Line go Up" })
 
-map("x", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true, desc = "Line go Down" })
-map("x", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true, desc = "Line go Up" })
+keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true, desc = "Line go Down" })
+keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true, desc = "Line go Up" })
 
-map("n", "<A-h>", "v<", { desc = "Line go Left" })
-map("n", "<A-l>", "v>", { desc = "Line go Right" })
+keymap("n", "<A-h>", "v<", { desc = "Line go Left" })
+keymap("n", "<A-l>", "v>", { desc = "Line go Right" })
 
-map("x", "<A-h>", "<gv", { desc = "Line go Left" })
-map("x", "<A-l>", ">gv", { desc = "Line go Right" })
+keymap("x", "<A-h>", "<gv", { desc = "Line go Left" })
+keymap("x", "<A-l>", ">gv", { desc = "Line go Right" })
 
--- buffer
-map("n", "<C-h>", "<C-w>h", { desc = "Window go Left" })
-map("n", "<C-j>", "<C-w>j", { desc = "Window go Down" })
-map("n", "<C-k>", "<C-w>k", { desc = "Window go Up" })
-map("n", "<C-l>", "<C-w>l", { desc = "Window go Right" })
+-- Move buffers
+keymap("n", "<C-h>", "<C-w>h", { desc = "Window go Left" })
+keymap("n", "<C-j>", "<C-w>j", { desc = "Window go Down" })
+keymap("n", "<C-k>", "<C-w>k", { desc = "Window go Up" })
+keymap("n", "<C-l>", "<C-w>l", { desc = "Window go Right" })
 
-map("t", "<C-h>", "<C-\\><C-N><C-w>h", { desc = "which_key_ignore" })
-map("t", "<C-j>", "<C-\\><C-N><C-w>j", { desc = "which_key_ignore" })
-map("t", "<C-k>", "<C-\\><C-N><C-w>k", { desc = "which_key_ignore" })
-map("t", "<C-l>", "<C-\\><C-N><C-w>l", { desc = "which_key_ignore" })
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", { desc = "which_key_ignore" })
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", { desc = "which_key_ignore" })
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", { desc = "which_key_ignore" })
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", { desc = "which_key_ignore" })
 
-map("t", "<C-x>", "<C-\\><C-N>", { desc = "Escape terminal mode" })
+keymap("t", "<C-x>", "<C-\\><C-N>", { desc = "Escape terminal mode" })
 
--- marks
-map("n", "dm", ":delmarks ", { desc = "Delete Marks" })
-
--- lsp
-map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
-map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+-- Marks
+keymap("n", "dm", ":delmarks ", { desc = "Delete Marks" })
